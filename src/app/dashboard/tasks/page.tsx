1f65b451d3dcee92e9d2327ea3b201d2b2687dae
rishabhbizzle <rishabhbizzle@gmail.com>
2024-07-14 20:34:22 +0530

    more changes

--- src/app/dashboard/tasks/page.tsx
+++ src/app/dashboard/tasks/page.tsx
@@ -1,16 +1,38 @@
-import React from 'react'
-import dynamic from 'next/dynamic';
-import ClientOnly from '@/components/ClientOnly';
+import React from "react";
+import dynamic from "next/dynamic";
+import { getUserTasks } from "@/lib/actions";
+const KanbanBoard = dynamic(() => import("./components/KanbanBoard"), {
+  ssr: false,
+});
 
-const KanbanBoard = dynamic(() => import('./components/KanbanBoard'), { ssr: false });
+const Page = async () => {
+  const { data: tasks, error } = await getUserTasks();
+  if (error) {
+    console.log(error);
+  }
 
-const Page = () => {
+  if (!tasks) {
+    return <div>Loading...</div>;
+  }
+
+  // restructure the data to match the format expected by the KanbanBoard component
+  const columns = {
+    "To Do": [],
+    "In Progress": [],
+    Done: [],
+    "On Hold": [],
+  };
+
+  tasks.forEach((task) => {
+    //@ts-ignore
+    columns[task.type].push(task);
+  });
+
   return (
-    <div className=''>
+    <div className="">
+      <KanbanBoard columns={columns} />
+    </div>
+  );
+};
 
-      <KanbanBoard />
-      </div>
-  )
-}
-
-export default Page
+export default Page;
--- src/app/dashboard/tasks/types.ts
+++ src/app/dashboard/tasks/types.ts
@@ -1,15 +1,18 @@
+import { UUID } from "crypto";
+
 // types/task.ts
 export interface Task {
-    id: string;
-    title: string;
-    description: string;
-    dueDate: string;
-    priority: 'High' | 'Medium' | 'Low';
-    type: string;
-  }
-  
-  export type ColumnId = 'To Do' | 'In Progress' | 'Done' | 'On Hold';
-  
-  export interface Columns {
-    [key: string]: Task[];
-  }
+  id?: UUID;
+  title: string;
+  description?: string;
+  due_date?: Date;
+  priority?: string;
+  category?: string;
+  type: ColumnId;
+}
+
+export type ColumnId = "To Do" | "In Progress" | "Done" | "On Hold";
+
+export interface Columns {
+  [key: string]: Task[];
+}
