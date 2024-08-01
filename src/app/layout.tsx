0dd69c9b38b791bb471a19a53ebdcd9a50d8119c
rishabhbizzle <rishabhbizzle@gmail.com>
2024-08-01 22:21:44 +0530

    feat: notes context provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
+import { NotesProvider } from "@/lib/context/NotesContext";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,14 +27,16 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        <NotesProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </NotesProvider>
       </body>
     </html>
   );
